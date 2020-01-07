fa8f131cdb718870c05c06c01d94789f9a3ce2d2
saharafathelbab <33489137+saharafathelbab@users.noreply.github.com>
2020-01-07 11:24:39 -0500

    minor edit

--- task-1-with-small-comments.dart
+++ task-1-with-small-comments.dart
@@ -1,10 +1,9 @@
 findName(String name){
   var friendList = ['mike', 'susan']; // friend list/array created of names
-  if(friendList.indexOf(name) < 0){ // if name is not in list, they get a standard greeting 'hi {username}'
-    friendList.add(name); // name is added to friendList list/array
-    print('hello ' + name);
-  }else{
+  if(friendList.contains(name){ 
     print('hi ' + name + ' great to see you!'); //if name is in list, they get a different greeting
+  }else{
+    print('hello ' + name); // if name is not in list, they get a standard greeting 'hi {username}'
   }
   
   print(friendList);
