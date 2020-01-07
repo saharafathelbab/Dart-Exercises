d0dbd86439c3312bd89534e5a283a87423db1809
saharafathelbab <33489137+saharafathelbab@users.noreply.github.com>
2020-01-07 10:17:22 -0500

    task 2 without a lot of comments

---
+++ task-2-without-a-lot-of-comments.dart
@@ -0,0 +1,11 @@
+findWages(int hoursWorked, double hourlyRate){
+  print(hoursWorked * hourlyRate); // multiplying hours worked by hourly rate
+  
+}
+
+void main() {
+  int worked = 10;
+  double rate = 22.45;
+  findWages(worked,rate);
+  
+}
