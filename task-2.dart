3bc31782df7db0dda1bde631668e77ebeab3ae27
saharafathelbab <33489137+saharafathelbab@users.noreply.github.com>
2020-01-07 09:50:06 -0500

    Task 2
    Dart Exercise 2

---
+++ task-2.dart
@@ -0,0 +1,21 @@
+//Task: Write a function that accepts an hourly rate as a floating point number and number of hours workd, then returns
+// the wages earned.
+
+//create a function that takes two arguments: an integer, the hours worked, and a floating point number, the hourly rate.
+// int = integer
+// double = used for decimals/floating point number
+
+findWages(int hoursWorked, double hourlyRate){
+
+// prints the amount of hours worked multiplied by the hourly rate
+
+  print(hoursWorked * hourlyRate);
+  
+}
+
+void main() {
+  int worked = 10;  // integer is declared, the amount of hours worked is 10
+  double rate = 22.45; // our floating point number is declared, the hourly rate is 22.45
+  findWages(worked,rate); // call our above function with our varaibles we declard above, worked and rate.
+  
+}
